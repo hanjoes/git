4799ac8c15e111c75bb914301aa233bd653a6eda
Hanzhou Shi <hanzhou87@gmail.com>
2019-06-15 16:54:01 -0700

    Adding travis upgraded pawn version.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["SwiftGitLib"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/hanjoes/swift-pawn", from: "0.2.1")
+        .package(url: "https://github.com/hanjoes/swift-pawn", from: "1.0.0")
     ],
     targets: [
         .target(
